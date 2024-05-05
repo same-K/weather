ba7f30319036ed3f203dc7829657f7fd03e515a4
same-K <>
2024-05-05 15:21:50 +0900

    サマリリスト用の実装を追加

---
+++ app/src/main/java/com/example/weather/model/SummaryItem.java
@@ -0,0 +1,16 @@
+package com.example.weather.model;
+
+import com.example.weather.database.Location;
+
+public class SummaryItem {
+
+    public int cityId;
+
+    public String cityName;
+    public long datetime;
+
+    public String icon;
+
+    public int temperature;
+
+}
