4fb4a7358145d0a03b207a2755abb16ea4308e96
same-K <>
2024-05-05 15:23:29 +0900

    起動後にホーム画面への遷移を追加

--- app/src/main/java/com/example/weather/view/WeatherActivity.java
+++ app/src/main/java/com/example/weather/view/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.weather.view;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -8,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.core.view.MenuProvider;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.navigation.NavController;
@@ -15,6 +17,7 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.weather.R;
 import com.example.weather.databinding.ActivityWeatherBinding;
 import com.google.android.material.appbar.MaterialToolbar;
 
@@ -22,14 +25,27 @@ public class WeatherActivity extends AppCompatActivity {
 
     private ActivityWeatherBinding binding;
 
+    public WeatherActivity() {
+        super(R.layout.activity_weather);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(savedInstanceState == null){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, HomeFragment.class, null)
+                    .setReorderingAllowed(true)
+                    .commit();
+        }
 
         binding = ActivityWeatherBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
+
     }
 
     private void setupMenuBar() {
